a9fe6522cdbc0d7c8e5e2331d92661e768c40b71
Greg Brunk <greg.brunk@gmail.com>
2016-08-15 13:02:00 -0600

    Solutions for all three exercises

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,35 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
+app.controller("ClassController", ClassController);
+
+function WelcomeController(){
+  this.full_name = "Greg Brunk";
+  this.age = 27;
+  this.city = "Denver";
+  this.state = "Colorado";
+  this.shout = function(phrase) {
+  	return phrase.toUpperCase() + "!";
+  };
+}
+
+function ClassController(){
+  this.class_name = "White Walkers";
+  this.enrolled_students = 16;
+  this.start_date = "6/15/16";
+  this.end_date = new Date(new Date().getFullYear() +0, 8, 2);
+  this.daysRemaining = function() {
+		// get total seconds between the times
+		var date_now = new Date();
+		var delta = Math.abs(this.end_date - date_now) / 1000;
+
+		// calculate (and subtract) whole days
+		var days = Math.floor(delta / 86400);
+		delta -= days * 86400;
+
+		return days;
+	};
+}
--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -1,9 +1,24 @@
 console.log('challenge #3 -- app.js loaded!');
 
-// var app = angular.module("appThree", []);
-// app.controller("infoController", infoController)
+var app = angular.module("appThree", []);
 
-// function infoController(){
-//   this.name = "Jon Doe"; // default name
-//   this.height = 5.6; // default height in feet
-// }
+app.controller("infoController", infoController);
+app.controller("loanController", loanController);
+
+function infoController(){
+  this.name = "Jon Doe"; // default name
+  this.height = 5.6; // default height in feet
+}
+
+function loanController(){
+	this.amount = 0;
+	this.apr = 0;
+	
+	this.month = function(months) {
+		var accrued_interest = (this.amount * (this.apr/100)) * months;
+		var total_balance = (this.amount * months) + accrued_interest;
+		return {
+			accrued_interest: accrued_interest, total_balance: total_balance
+		};
+	};
+}
